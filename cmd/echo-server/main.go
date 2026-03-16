c183d48e2078afbf5634532bb8edc361374113a7
The Crypto Donkey <cryptofire365@gmail.com>
2026-03-16 17:24:20 +0000

    feat: add minimal echo server for Aperture demo

---
+++ cmd/echo-server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := ":8080"
+	if port := os.Getenv("PORT"); port != "" {
+		addr = ":" + port
+	}
+
+	http.HandleFunc("/v1/echo", func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil || len(body) == 0 {
+			fmt.Fprintln(w, "hello")
+			return
+		}
+		w.Write(body)
+	})
+
+	fmt.Fprintf(os.Stderr, "echo-server listening on %s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Fprintf(os.Stderr, "echo-server: %v\n", err)
+		os.Exit(1)
+	}
+}
